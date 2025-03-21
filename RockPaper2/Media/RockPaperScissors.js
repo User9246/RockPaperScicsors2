c463452ef2cbabb0ef2655f8f677ec0a278c5640
User9246 <164345567+User9246@users.noreply.github.com>
2025-03-21 14:32:17 +0000

    Game logic 0.1

--- RockPaper2/Media/RockPaperScissors.js
+++ RockPaper2/Media/RockPaperScissors.js
@@ -1,9 +1,18 @@
 // 0 = rock, 1 = paper, 2 = scissors, 3 = shoot
 const CompOutputText = document.getElementById("ComputerOutput");
+const PlayerForwardInputText = document.getElementById("PlayerForwardInput");
+const WinStatusText = document.getElementById("WinStatus");
 let ComputerChoice;
 let ComputerChoiceString;
+let GameInfo = {
+    'ComputerString': null,
+    'ComputerNum': null,
+    'PlayerInput': null
+}
 let GameOps = {
-    
+    'GameWins': 0,
+    'GameLose': 0,
+    'GameTies': 0
 }
 
 function CreateMatch(PlayerInput){
@@ -26,11 +35,22 @@ function CreateMatch(PlayerInput){
             console.log("ComputerChoiceString SWITCH, is the index out of rage?");
             break;
     }
-    console.log({
+    GameInfo = {
         'ComputerString': ComputerChoiceString,
         'ComputerNum': ComputerChoice,
         'PlayerInput': PlayerInput
-    });
-    ComputerOutput.textContent = `Comp: ${ComputerChoiceString}, Player: ${PlayerInput}`;
+    };
+    console.log(GameInfo);
+    console.log(GameOps);
+    WinStatusText.textContent = `^${PlayerInput}^`;
     
+    if(ComputerChoice == "rock" && PlayerInput == "Paper" || PlayerInput == "Shoot"){
+        //Players wins game
+        CompOutputText = `Computer: ${ComputerChoiceString}`;
+        GameOps.GameWins += 1;
+    }
+    else{
+        CompOutputText = "[ERROR]"
+    }
+    WinStatusText.textContent = `${GameOps.GameWins} Wins, ${GameOps.GameTies} Ties, ${GameOps.GameLose} Loses`;
 }
