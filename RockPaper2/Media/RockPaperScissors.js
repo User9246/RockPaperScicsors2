4629df62744a838100c6376fe9358b397e20a14f
User9246 <164345567+User9246@users.noreply.github.com>
2025-03-28 14:03:44 +0000

    error prev

--- RockPaper2/Media/RockPaperScissors.js
+++ RockPaper2/Media/RockPaperScissors.js
@@ -16,58 +16,62 @@ let GameOps = {
 }
 
 function CreateMatch(PlayerInput){
-    ComputerChoice = Math.floor(Math.random() * 3);
-
-    switch (ComputerChoice){
-        case 0:
-            ComputerChoiceString = "Rock";
-            break;
-        case 1:
-            ComputerChoiceString = "Paper";
-            break;
-        case 2:
-            ComputerChoiceString = "Scissors";
-            break;
-        case 3:
-            ComputerChoiceString = "Shoot";
-        default:
-            ComputerChoiceString = "[ERROR]";
-            console.log("ComputerChoiceString SWITCH, is the index out of rage?");
-            break;
-    }
-    GameInfo = {
-        'ComputerString': ComputerChoiceString,
-        'ComputerNum': ComputerChoice,
-        'PlayerInput': PlayerInput
-    };
-    console.log(GameInfo);
-    console.log(GameOps);
-    PlayerForwardInputText.textContent = `^${PlayerInput}^`;
-    
-    if(ComputerChoiceString == "Rock" && (PlayerInput == "Paper" || PlayerInput == "Shoot")){
-        //Players wins game
-        CompOutputText.textContent = `Computer: ${ComputerChoiceString}`;
-        GameOps.GameWins += 1;
-    }
-    else if(ComputerChoiceString == "Paper" && (PlayerInput == "Scissors" || PlayerInput == "Shoot")){
-        //Players wins game
-        CompOutputText.textContent = `Computer: ${ComputerChoiceString}`;
-        GameOps.GameWins += 1;
-    }
-    else if(ComputerChoiceString == "Scissors" && (PlayerInput == "Rock" || PlayerInput == "Shoot")){
-        //Players wins game
-        CompOutputText.textContent = `Computer: ${ComputerChoiceString}`;
-        GameOps.GameWins += 1;
-    }
-    else if(ComputerChoiceString == PlayerInput){
-        //Tie game
-        CompOutputText.textContent = `Computer: ${ComputerChoiceString}`;
-        GameOps.GameTies += 1;
+    if(PlayerInput == "Rock" || "Paper" || "Scissors"){
+        ComputerChoice = Math.floor(Math.random() * 3);
+        switch (ComputerChoice){
+            case 0:
+                ComputerChoiceString = "Rock";
+                break;
+            case 1:
+                ComputerChoiceString = "Paper";
+                break;
+            case 2:
+                ComputerChoiceString = "Scissors";
+                break;
+            case 3:
+                ComputerChoiceString = "Shoot";
+            default:
+                ComputerChoiceString = "[ERROR]";
+                console.log("ComputerChoiceString SWITCH, is the index out of rage?");
+                break;
+        }
+        GameInfo = {
+            'ComputerString': ComputerChoiceString,
+            'ComputerNum': ComputerChoice,
+            'PlayerInput': PlayerInput
+        };
+        console.log(GameInfo);
+        console.log(GameOps);
+        PlayerForwardInputText.textContent = `^${PlayerInput}^`;
+        
+        if(ComputerChoiceString == "Rock" && (PlayerInput == "Paper" || PlayerInput == "Shoot")){
+            //Players wins game
+            CompOutputText.textContent = `Computer: ${ComputerChoiceString}`;
+            GameOps.GameWins += 1;
+        }
+        else if(ComputerChoiceString == "Paper" && (PlayerInput == "Scissors" || PlayerInput == "Shoot")){
+            //Players wins game
+            CompOutputText.textContent = `Computer: ${ComputerChoiceString}`;
+            GameOps.GameWins += 1;
+        }
+        else if(ComputerChoiceString == "Scissors" && (PlayerInput == "Rock" || PlayerInput == "Shoot")){
+            //Players wins game
+            CompOutputText.textContent = `Computer: ${ComputerChoiceString}`;
+            GameOps.GameWins += 1;
+        }
+        else if(ComputerChoiceString == PlayerInput){
+            //Tie game
+            CompOutputText.textContent = `Computer: ${ComputerChoiceString}`;
+            GameOps.GameTies += 1;
+        }
+        else{
+            //COMPUTER wins
+            CompOutputText.textContent = `Computer: ${ComputerChoiceString}`;
+            GameOps.GameLose += 1;
+        }
+        WinStatusText.textContent = `${GameOps.GameWins} Wins, ${GameOps.GameTies} Ties, ${GameOps.GameLose} Loses`;
     }
     else{
-        //COMPUTER wins
-        CompOutputText.textContent = `Computer: ${ComputerChoiceString}`;
-        GameOps.GameLose += 1;
+        console.log("Invalid item");
     }
-    WinStatusText.textContent = `${GameOps.GameWins} Wins, ${GameOps.GameTies} Ties, ${GameOps.GameLose} Loses`;
 }
